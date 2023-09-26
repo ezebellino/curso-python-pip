375a1a4b948beec6c51a7430ffe3d7c1ea19407d
ezebellino <ezebellino@hotmail.com>
2023-09-26 14:32:52 -0300

    se modificaron app/main.py y app/pie.png

--- app/main.py
+++ app/main.py
@@ -1,18 +1,29 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
-  data = read_csv.read_csv('data.csv')
+
+# El código de abajo es sin la implementación de PANDAS. Lo que nos sirve para entender;
+# y aprender los algoritmos, instrucciones y estructuras a utilizar sin necesidad de un módulo.
+  """  data = read_csv.read_csv('data.csv')
   #De la siguiente manera podemos filtrarlos de manera tal que queden sólo países de sudamérica
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  """
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
 
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
 
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('data.csv') # esta línea se reutiliza para no corromper con el código
   country = input('Type country please => ')
   print(country)
 
